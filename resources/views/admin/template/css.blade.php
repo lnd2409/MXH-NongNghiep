97a648d7b486ed7889e9b700130fde51076a5952
unknown <lnd240998@gmail.com>
2020-05-12 00:34:48 +0700

    update admin template

---
+++ resources/views/admin/template/css.blade.php
@@ -0,0 +1,14 @@
+<!-- plugins:css -->
+<link rel="stylesheet" href="{{ asset('admin-template') }}/vendors/mdi/css/materialdesignicons.min.css">
+<link rel="stylesheet" href="{{ asset('admin-template') }}/vendors/simple-line-icons/css/simple-line-icons.css">
+<link rel="stylesheet" href="{{ asset('admin-template') }}/vendors/flag-icon-css/css/flag-icon.min.css">
+<link rel="stylesheet" href="{{ asset('admin-template') }}/vendors/css/vendor.bundle.base.css">
+<!-- endinject -->
+<!-- plugin css for this page -->
+<link rel="stylesheet" href="{{ asset('admin-template') }}/vendors/font-awesome/css/font-awesome.min.css">
+<link rel="stylesheet" href="{{ asset('admin-template') }}/vendors/jquery-bar-rating/fontawesome-stars.css">
+<!-- End plugin css for this page -->
+<!-- inject:css -->
+<link rel="stylesheet" href="{{ asset('admin-template') }}/css/style.css">
+<!-- endinject -->
+<link rel="shortcut icon" href="{{ asset('admin-template') }}/images/favicon.png">
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin/trang-chu', function () {
+    return view('admin.index');
+});
