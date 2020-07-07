57df5ef278079c3c3182d0d91a6a97dda3b90b27
unknown <lqbinhhere1998@gmail.com>
2020-07-07 11:11:04 +0700

    update code 07/07/2020

--- app/Http/Controllers/SanphamnuoitrongController.php
+++ app/Http/Controllers/SanphamnuoitrongController.php
@@ -63,8 +63,8 @@ class SanphamnuoitrongController extends Controller
         $nongdan = \Auth::guard('nongdan')->user()->nd_id;
         $now = Carbon::now();
         
-        if($request->ten=='' || $request->thongtin=='' || $request->thangbatdau=='' || $request->thangketthuc=='' || $request->sanluongdutinh=='' || $request->donvitinh=='' || $request->soluongnongsan=='' ){
-            $success = Session::put('alert-del', 'Tên sản phẩm nuôi trống không được trống');
+        if($request->tenNongSan=='' || $request->thongTinNongSan=='' || $request->thangBatDau=='' || $request->thangKetThuc=='' || $request->sanLuongDuTinh=='' || $request->donvitinh=='' || $request->soluongnongsan=='' ){
+            $success = Session::put('alert-del', 'Dữ liệu không được trống');
             return redirect()->back();
         }
        
@@ -253,6 +253,7 @@ class SanphamnuoitrongController extends Controller
                     
                     'qm_soluongnongsan' => $request->soluongnongsan,
                     'quymo.dvt_id' => $request->donvitinh,
+                    
                     'quymo.updated_at' => $now,
                     
                 ]
